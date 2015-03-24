34d84f314f240cfb2248c20c882fe5ea0872ba5a
Benjamin Hofner <benjamin.hofner@fau.de>
2015-03-24 16:17:20 +0100

    updated simulation code

--- inst/simEx.R
+++ inst/simEx.R
@@ -1,19 +1,16 @@
-
-library(DatABEL) # genotype data objects
-library(Matrix)
-library(lattice) # levelplot
 library(kangar00)
 library(mboostDevel)
 
 ##### make GWASobjekt for simulated data #####################
-path.sim <- "..."
+path.sim <- "data/"
 anno    <- read.table(paste(path.sim,"sim.6pw.anno.txt",sep=""), header=T, as.is=T)
 pheno   <- read.table(paste(path.sim,"sim2.pheno.txt",sep=""), header=T, as.is=T)
 dat     <- databel(paste(path.sim,"sim2",sep=""))
 attr(dat, "anno") <- anno
 
-sim2 <- GWASdata(pheno=pheno, geno=dat, desc="simulation2.6pathways")
-save(sim2,file="sim2.6pw.GWAS.RData")
+sim2 <- GWASdata(pheno = pheno, geno = dat,
+                 desc = "simulation2.6pathways")
+save(sim2,file="data/sim2.6pw.GWAS.RData")
 
 
 ### load prepared data #########################################################
@@ -41,50 +38,24 @@ hsa00780
 summary(hsa00780)
 plot(hsa00780)
 
-## # linear kernel
-## kern <- kernel.lin(sim, pnet)
-## plot(kern)
-## kern
-## summary(kern)
-##
-## # size-adjusted
-## kern <- kernel.sia(sim,pnet)
-## kern
-## summary(kern)
-## plot(kern)
-##
-## # network kernel
-## kern <- kernel.net(sim,pnet)
-## summary(kern)
-## plot(kern)
-## plot(kern,hclust=TRUE)
-##
-## # define LKMTtest
-## source('kangar00/R/lkmt.r')
-## m <- lkmt(pheno ~ 1+sex+age, kern, sim)
-##
-## m
-## summary(m)
-##
-##
-## #### now use linear kernel with mboost
-## library("mboostDevel")
-##
-## z <- sim@geno
-## attr(z, "anno") <- sim@anno
-##
-## ## SNPset <-sim@anno$rsNumber[which(sim@anno$Pathway==pnet@id)]
-## ## z <- as(as(sim@geno[,as.character(SNPset)],'matrix'), "Matrix")
-##
-## data$pheno <- as.factor(data$pheno)
-## data$sex <- as.factor(data$sex)
-## data$z <- z
-## attr(data$z, "anno")
-##
-## # ND <- data[1:5, ]
-## # str(ND)
-##
-## mod <- gamboost(pheno ~ bkern(z, kernel = kernel.lin, pathway = pnet, args =
-##                                   list()) + sex + age,
-##                 data = data, family = Binomial())
-## plot(mod)
+#### now use linear kernel with mboost
+
+## SNPset <-sim@anno$rsNumber[which(sim@anno$Pathway==pnet@id)]
+## z <- as(as(sim@geno[,as.character(SNPset)],'matrix'), "Matrix")
+
+data <- sim2@pheno
+
+data$pheno <- as.factor(data$pheno)
+data$sex <- as.factor(data$sex)
+data$z <- sim2@geno
+attr(data$z, "anno")
+
+mod <- gamboost(pheno ~ sex + age +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa00100, args = list()) +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa00603, args = list()) +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa04122, args = list()) +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa00750, args = list()) +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa00730, args = list()) +
+                  bkernel(z, kernel = kernel.lin, pathway = hsa00780, args = list()),
+                data = data, family = Binomial())
+
